2a7638851da99f4a201db4d19e1a16515eaffe70
haha <gxd2013@outlook.com>
2019-03-30 11:06:44 +0800

    添加分卷细节界面

---
+++ Writer/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Writer.ViewModels
+{
+    internal sealed class ItemDetailViewModel : NotifyPropertyChanged
+    {
+        public WritableTextBlockViewModel Title { get; } = new WritableTextBlockViewModel();
+        public WritableTextBlockViewModel Content { get; } = new WritableTextBlockViewModel();
+    }
+}
