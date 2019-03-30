1fbb14cafde85b6026832b083d2ce16c1f3d84c1
haha <gxd2013@outlook.com>
2019-03-30 20:40:11 +0800

    重构作品界面

---
+++ Writer/General.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Writer.ViewModels;
+
+namespace Writer
+{
+    internal sealed class General
+    {
+        /// <summary>
+        /// 当前处于编辑状态，则为 true；添加状态，则为 false。
+        /// </summary>
+        public bool InEdit { get; set; }
+
+        /// <summary>
+        /// 未编辑前的文本。
+        /// </summary>
+        public string LastText { get; private set; }
+
+        public void AddOrEdit(Action Edit, Action Add)
+        {
+            if (InEdit) Edit?.Invoke();
+            else          Add?.Invoke();
+        }
+
+        public void BeginEdit(WritableTextBlockViewModel tb)
+        {
+            tb.IsReadOnly = false;
+            LastText = tb.Text;
+        }
+
+        public void EndEdit(WritableTextBlockViewModel tb)
+        {
+            tb.IsReadOnly = true;
+            ClearError(tb);
+        }
+
+        public void CancelEditOrAdd(WritableTextBlockViewModel tb, Action cancelAdd)
+        {
+            if (InEdit)
+            {
+                EndEdit(tb);
+            }
+            else
+            {
+                cancelAdd?.Invoke();
+            }
+        }
+
+        public void SetError(WritableTextBlockViewModel tb, string v)
+        {
+            tb.InError = true;
+            tb.Error = v;
+        }
+
+        public void ClearError(WritableTextBlockViewModel tb)
+        {
+            tb.InError = true;
+            tb.Error = null;
+        }
+    }
+}
--- Writer/StoryBooks.xaml.cs
+++ Writer/StoryBooks.xaml.cs
@@ -17,10 +17,9 @@ namespace Writer
     public sealed partial class StoryBooks : Page
     {
         private readonly Story m_story;
-        private readonly ObservableCollection<BookViewModel> m_list;
-        private BookViewModel m_currentBook;
-        private string m_lastName;
-        private bool m_isEdit;
+        private readonly ObservableCollection<BookViewModel> m_list; // 作品的集合。
+        private readonly General m_general; //通用操作。
+        private BookViewModel m_current;    // 当前作品。
 
         public StoryBooks()
         {
@@ -28,6 +27,7 @@ namespace Writer
 
             m_story = new Story();
             m_list = new ObservableCollection<BookViewModel>();
+            m_general = new General();
 
             Books.ItemsSource = m_list;
 
@@ -52,40 +52,40 @@ namespace Writer
         // 添加新作品。
         private void AddNewBook_Click(object sender, RoutedEventArgs e)
         {
-            m_isEdit = false;
+            m_general.InEdit = false;
 
-            m_currentBook = new BookViewModel();
-            m_list.Add(m_currentBook);
+            m_current = new BookViewModel();
+            m_list.Add(m_current);
 
-            BeginEditBookName();
+            m_general.BeginEdit(m_current.Title);
         }
 
         private void CreateNewBook()
         {
-            if (m_currentBook == null)
-                throw new ArgumentNullException(nameof(m_currentBook));
+            if (m_current == null)
+                throw new ArgumentNullException(nameof(m_current));
 
-            var bookName = m_currentBook.Title.Text;
+            var bookName = m_current.Title.Text;
             if (string.IsNullOrEmpty(bookName))
             {
-                SetError("请输入新作品的书名");
+                m_general.SetError(m_current.Title, "请输入新作品的书名");
                 return;
             }
 
             if (m_story.Contains(bookName))
             {
-                SetError($"作品【{bookName}】已存在");
+                m_general.SetError(m_current.Title, $"作品【{bookName}】已存在");
                 return;
             }
 
             var m = m_story.AddBook(bookName);
             if (m != null)
             {
-                m_currentBook.SetModel(m);
+                m_current.SetModel(m);
             }
 
             SetBookCount(m_list.Count);
-            EndEditBookName();
+            m_general.EndEdit(m_current.Title);
         }
 
         #region 编辑图书名
@@ -93,8 +93,8 @@ namespace Writer
         // 开始编辑书名。
         private void BeginEditBookName_Click(object sender, RoutedEventArgs e)
         {
-            m_isEdit = true;
-            BeginEditBookName();
+            m_general.InEdit = true;
+            m_general.BeginEdit(m_current.Title);
         }
 
         // 添加、编辑或取消。
@@ -102,87 +102,51 @@ namespace Writer
         {
             if (e.Key == VirtualKey.Escape)
             {
-                CancelEdit();
+                m_general.CancelEditOrAdd(m_current.Title, () =>
+                {
+                    m_list.Remove(m_current);
+                    SetBookCount(m_list.Count);
+                });
             }
 
             if (e.Key == VirtualKey.Enter)
             {
-                AddOrEditBook();
+                m_general.AddOrEdit(EditBookName, CreateNewBook);
             }
-        }
-
-        private void AddOrEditBook()
-        {
-            if (m_isEdit)
-                EditBookName();
-            else
-                CreateNewBook();
-        }
-
-        private void BeginEditBookName()
-        {
-            m_currentBook.Title.IsReadOnly = false;
-            m_lastName = m_currentBook.Title.Text;
         }
 
         private void EditBookName()
         {
-            if (m_currentBook == null || m_lastName == null)
-                throw new ArgumentNullException((m_currentBook == null) ? nameof(m_currentBook) : nameof(m_lastName));
+            if (m_current == null || m_general.LastText == null)
+                throw new ArgumentNullException((m_current == null) ? nameof(m_current) : nameof(m_general.LastText));
 
-            if (m_currentBook.Title.IsReadOnly) return;
+            if (m_current.Title.IsReadOnly) return;
 
-            var newName = m_currentBook.Title.Text;
+            var newName = m_current.Title.Text;
             if (string.IsNullOrEmpty(newName))
             {
-                SetError("请输入新的作品名");
+                m_general.SetError(m_current.Title, "请输入新的作品名");
                 return;
             }
 
-            if (newName == m_lastName)
+            if (newName == m_general.LastText)
             {
-                SetError("作品名并未修改");
+                m_general.SetError(m_current.Title, "作品名并未修改");
                 return;
             }
 
             if (m_story.Contains(newName))
             {
-                SetError($"作品【{newName}】已经存在");
+                m_general.SetError(m_current.Title, $"作品【{newName}】已经存在");
                 return;
             }
 
-            var book = m_currentBook;
+            var book = m_current;
             book.Title.Text = newName;
             if (m_story.Update(book.Model))
-                EndEditBookName();
+                m_general.EndEdit(m_current.Title);
             else
-                SetError($"保存作品【{newName}】失败");
-        }
-
-        private void EndEditBookName()
-        {
-            m_currentBook.Title.IsReadOnly = true;
-            m_currentBook.Title.InError = false;
-            m_currentBook.Title.Error = null;
-        }
-
-        private void CancelEdit()
-        {
-            if (m_isEdit)
-            {
-                EndEditBookName();
-            }
-            else
-            {
-                m_list.Remove(m_currentBook);
-                SetBookCount(m_list.Count);
-            }
-        }
-
-        private void SetError(string v)
-        {
-            m_currentBook.Title.InError = true;
-            m_currentBook.Title.Error = v;
+                m_general.SetError(m_current.Title, $"保存作品【{newName}】失败");
         }
 
         #endregion
@@ -190,7 +154,7 @@ namespace Writer
         // 删除作品。
         private async void Delete_Click(object sender, RoutedEventArgs e)
         {
-            var book = m_currentBook.Model;
+            var book = m_current.Model;
             var r = await Message.ShowWarningAsync(
                 $"删除作品【{book.Title}】后，该作品的相关内容也将一并删除。该操作**无法**恢复！\n\n删除【{book.Title}】吗？",
                 "是的");
@@ -202,7 +166,7 @@ namespace Writer
             }
             else
             {
-                m_list.Remove(m_currentBook);
+                m_list.Remove(m_current);
                 SetBookCount(m_list.Count);
             }
         }
@@ -210,7 +174,7 @@ namespace Writer
         // 进入大纲视图。
         private void Outline_Click(object sender, RoutedEventArgs e)
         {
-            var book = m_currentBook;
+            var book = m_current;
             Frame.Navigate(typeof(Storyboard), book);
         }
 
@@ -219,14 +183,14 @@ namespace Writer
         {
             if (e.AddedItems.Count == 0) return;
 
-            m_currentBook = e.AddedItems[0] as BookViewModel;
+            m_current = e.AddedItems[0] as BookViewModel;
             Outline_Click(null, null);
         }
 
         // 右键选中作品。
         private void Books_RightTapped(object sender, RightTappedRoutedEventArgs e)
         {
-            m_currentBook = (e.OriginalSource as FrameworkElement).DataContext as BookViewModel;
+            m_current = (e.OriginalSource as FrameworkElement).DataContext as BookViewModel;
         }
     }
 }
